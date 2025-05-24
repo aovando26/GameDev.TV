fcbd0c3901b743961172362b4f944d773e2176ed
aovando <aovandodevsxr@gmail.com>
2025-05-23 23:05:21 -0400

    raindrops spawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public bool gameIsActive;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject rainDrops;
+    [SerializeField]
+    private int rainDropsNumber = 1;
+    private float spawnRange = 6.0f;
+    private float spawnCooldown = 1.0f;
+    private float timeSinceLastSpawn = 0f;
+
+    private void Start()
+    {
+        SpawnRainDropWave(rainDropsNumber);
+    }
+
+    private void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        // Only spawn if there are no raindrops and cooldown has passed
+        if (GameObject.FindGameObjectsWithTag("Raindrop").Length == 0 && timeSinceLastSpawn >= spawnCooldown)
+        {
+            SpawnRainDropWave(rainDropsNumber);
+            timeSinceLastSpawn = 0f;
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float randomX = Random.Range(-spawnRange, spawnRange);
+        float randomZ = Random.Range(-spawnRange, spawnRange);
+        return new Vector3(randomX, 5, randomZ);
+    }
+
+    private void SpawnRainDropWave(int rainToSpawn)
+    {
+        for (int i = 0; i < rainToSpawn; i++)
+        {
+            Instantiate(rainDrops, GenerateSpawnPosition(), rainDrops.transform.rotation);
+            Debug.Log("Instantiated raindrop " + i);
+        }
+    }
+}
